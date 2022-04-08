71715cb3237bc0efea9caaace18c1be8f8e5874e
JToo13 <toojoey57@gmail.com>
2022-04-08 08:15:04 -0700

    add new script

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		putchar((n % 10) + '0');
+		n++;
+		if (n != 10)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
