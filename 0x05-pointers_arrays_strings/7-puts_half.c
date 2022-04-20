13c718814974a438a5c99b5498d03ccdac9ec84b
JToo13 <toojoey57@gmail.com>
2022-04-20 13:31:20 -0700

    add new script

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * followed by a new line
+ * @str: string to be printed
+ */
+void puts_half(char *str)
+{
+	int i, j;
+
+	i = 0;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	if (i % 2 == 0)
+	{
+		for (j = i / 2; str[j] != '\0'; j++)
+		{
+			_putchar(str[j]);
+		}
+	}
+	else
+	{
+		for (j = (i - 1) / 2; str[j] != '\0'; j++)
+		{
+			_putchar(str[j]);
+		}
+	}
+	_putchar('\n');
+}
