e8feabecf49bcdcc560b3033b70476d9c96db718
JToo13 <toojoey57@gmail.com>
2022-04-21 09:05:06 -0700

    changes to the script

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -17,14 +17,14 @@ void puts_half(char *str)
 	}
 	if (i % 2 == 0)
 	{
-		for (j = i / 2; str[j] != '\0'; j++)
+		for (j = i / 2; j < i; j++)
 		{
 			_putchar(str[j]);
 		}
 	}
 	else
 	{
-		for (j = (i - 1) / 2; str[j] != '\0'; j++)
+		for (j = (i - 1) / 2; j < i; j++)
 		{
 			_putchar(str[j + 1]);
 		}
