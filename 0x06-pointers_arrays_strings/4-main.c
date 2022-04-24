889cda6484b178164d859e1d3431beabf4e81a11
JToo13 <toojoey57@gmail.com>
2022-04-24 07:02:42 -0700

    add new script

--- 0x06-pointers_arrays_strings/4-main.c
+++ 0x06-pointers_arrays_strings/4-main.c
@@ -3,10 +3,9 @@
 
 /**
  * main - check the code
+ * print_array - prints an array of integers
  * @a: an array of integers
  * @n: the number of elements to swap
- *
- * Return: nothing.
  */
 void print_array(int *a, int n)
 {
