db46b27eb915580ddb0cb4b4cb47a7f82ed05e0f
JToo13 <toojoey57@gmail.com>
2022-05-10 10:43:04 -0700

    add new script

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,6 +15,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	int i, j, k, l;
 	char *p;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	for (i = 0; s1[i] != '\0'; i++)
 	{
 		;
@@ -23,7 +27,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		;
 	}
-	p = malloc(sizeof(*p) * (i + j + 1));
+	p = malloc(sizeof(*p) * (i + n + 1));
 	if (p == NULL)
 	{
 		return (NULL);
